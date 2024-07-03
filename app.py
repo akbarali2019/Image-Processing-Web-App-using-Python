bdd59e0140b44194fff94664f65cddab2e2c3d30
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-07-03 19:02:39 +0900

    app py commits

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ external_stylesheets = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css'
 ]
 
+###########################################
+#external_stylesheets = [
+#    'https://codepen.io/chriddyp/pen/bWLwgP.css'
+#]
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.config['suppress_callback_exceptions'] = True
 app.title = 'Image Processing App'
